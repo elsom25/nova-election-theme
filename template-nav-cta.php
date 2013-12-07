cef89efa6c7f7dbf6633a88172b46fa355cd7d37
Jesse McGinnis <jessieelsom@hotmail.com>
2013-12-07 02:15:43 -0500

    Fixed CTA. Temporarily focing CTA on.
    Former-commit-id: fdf2e9c275f0d69243eb3a5d7980e16eb2f488e2
    Former-commit-id: eb0dad1eeb1058ee5cde742d8f035d35a0b27d3a

--- template-nav-cta.php
+++ template-nav-cta.php
@@ -1,3 +1,3 @@
 <div class="vote-cta text-center acts-as-link" id="vote-cta" style="display:none;">
-  <a href="//vote.feds.ca/" target="_blank">Vote Team Gold Now!</a>
+  <a href="//vote.feds.ca/" target="_blank">Vote <?php bloginfo('name'); ?> Now!</a>
 </div>
