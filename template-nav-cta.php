e7ae969c478e2cfb0827445db9e7e98e416a6643
Jesse McGinnis <jessieelsom@hotmail.com>
2014-01-15 01:19:20 -0500

    Made vote bar js link work again.
    Former-commit-id: 9b0e0d9b021248d68d1aad50029b77e71d741927
    Former-commit-id: 925559c1a3fda521b3a5e4fe557c260cdc9bcd34

--- template-nav-cta.php
+++ template-nav-cta.php
@@ -1,3 +1,3 @@
-<div class="vote-cta text-center acts-as-link" id="vote-cta" style="display:none;">
+<div class="vote-cta text-center" id="vote-cta" style="display:none;">
   <a href="#" data-reveal-id="voteModal">Vote <?php bloginfo('name'); ?> Now!</a>
 </div>
